18ebeb8f55a4a68c1f874a0daec249880c69dd43
QaziShahijahan <qazishahijahan123@gmail.com>
2025-04-10 21:04:50 +0530

    Navbar component modified and changed the links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='w-full bg-gray-900 text-white shadow-md py-4'>
-         <div className='flex justify-start gap-10 text-lg font-semibold ms-5'>
-        <NavLink to={"/"}>
-            Home 
+    <div className="w-full bg-gray-900 text-white shadow-md py-4">
+      <div className="flex justify-start gap-10 text-lg font-semibold ms-5">
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `hover:text-blue-400 transition duration-200 ${
+              isActive ? "text-blue-500 " : ""
+            }`
+          }
+        >
+          Home
         </NavLink>
-        <NavLink to={"/pastes"}>
-            Pastes 
+        <NavLink
+          to={"/pastes"}
+          className={({ isActive }) =>
+            `hover:text-blue-400 transition duration-200 ${
+              isActive ? "text-blue-500 " : ""
+            }`
+          }
+        >
+          Pastes
         </NavLink>
+      </div>
     </div>
-    </div>
-   
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
